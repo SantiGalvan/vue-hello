61efaf4ffdca0ecd3d0f4838f9b3a7bef72b85f3
SantiGalvan <santiagogalvancolorado@gmail.com>
2024-01-23 15:30:40 +0100

    add h1 & image

--- js/script.js
+++ js/script.js
@@ -1,1 +1,13 @@
 console.log('JS OK', Vue);
+
+// Attivo Vue
+const { createApp } = Vue;
+const app = createApp({
+    data() {
+        return {
+            name: 'Santiago',
+            image: 'https://d1csarkz8obe9u.cloudfront.net/posterpreviews/joystick-design-template-0d27cd04c9dca58fa3516fde1a7250f4_screen.jpg?ts=1626869886'
+        }
+    }
+});
+app.mount('#root');
